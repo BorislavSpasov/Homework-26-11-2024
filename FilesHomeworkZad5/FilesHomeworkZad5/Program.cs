aa71d1624b1b46eff8930e3e2be9a26f1bc4aff3
BorislavSpasov <borislav.e.spasov@gmail.com>
2024-12-02 22:46:33 +0200

    Add files via upload

---
+++ FilesHomeworkZad5/FilesHomeworkZad5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+Console.Write("CSV file location: ");
+string path = Console.ReadLine();
+Console.Write("New file location: ");
+string newFileLoc = Console.ReadLine();
+
+string csvLine = null;
+
+int productLoc = 0;
+int quantityLoc = 1;
+int priceLoc = 2;
+
+string mostSoldProduct = null;
+int mostSoldProductValue = 0;
+
+double totalPrice = 0;
+double test = 1.2;
+
+StreamReader srCSV = new StreamReader(path);
+StreamWriter swNewFile = new StreamWriter(newFileLoc);
+while (!srCSV.EndOfStream)
+{
+    csvLine = srCSV.ReadLine();
+    string[] currentLine = csvLine.Split(',');
+    if (int.Parse(currentLine[quantityLoc]) > mostSoldProductValue)
+    {
+        mostSoldProductValue = int.Parse(currentLine[quantityLoc]);
+        mostSoldProduct = currentLine[productLoc];
+    }
+    totalPrice += double.Parse(currentLine[quantityLoc]) * double.Parse(currentLine[priceLoc], CultureInfo.InvariantCulture);
+}
+
+swNewFile.WriteLine("Total Price Of Products: " + totalPrice);
+swNewFile.WriteLine("The Most Sold Product: " + mostSoldProduct);
+swNewFile.Flush();
---
+++ FilesHomeworkZad6/FilesHomeworkZad6/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Log File location: ");
+string path = Console.ReadLine();
+StreamWriter sw = new StreamWriter(path);
+
+if (File.Exists(path))
+{
+    Console.Write("Calculate factorial: ");
+    int input = int.Parse(Console.ReadLine());
+    int result = 1;
+    var watch = System.Diagnostics.Stopwatch.StartNew();
+    double elapsedMs = 0;
+    for (int i = input; i > 0; i--)
+    {
+        result *= i;
+    }
+    elapsedMs = watch.ElapsedTicks;
+    watch.Stop();
+    DateTime now = DateTime.Now;
+    sw.WriteLine($"Ticks for execution: {elapsedMs}; Current Date and Time: {now}; Result: {result}");
+    sw.Flush();
+}
